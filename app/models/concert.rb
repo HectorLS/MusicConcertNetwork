b5c893d4f68566fc803d654ab6475a1b1dad6f62
Hector Leon <hectorleon.designer@gmail.com>
2015-02-06 12:57:09 +0100

    FUcking BOOTSTRAP

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,2 +1,11 @@
 class Concert < ActiveRecord::Base
+	has_many :comments, dependent: :delete_all
+
+	validates :band, presence: true
+	validates :description, presence: true, length: { in: 10..300 }
+	validates :city, presence: true
+	validates :venue, presence: true
+	validates :date, presence: true
+	validates :price, presence: true, numericality: true
+
 end
